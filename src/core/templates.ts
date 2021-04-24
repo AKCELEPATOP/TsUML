9a05c17c3e88dd3f42a3f6a9832f3083dcdbd2e7
Alexsander Zemlyanskiy <sasha98zem@gmail.com>
2021-04-24 15:33:36 +0400

    adopt templates for new api version

--- src/core/templates.ts
+++ src/core/templates.ts
@@ -1,34 +1,51 @@
-import { PropertyDetails, MethodDetails} from "./interfaces";
+import {PropertyDetails, MethodDetails} from "./interfaces";
+
+const LINE_SEPARATOR = ";";
+
+const buildEntity = (name: string, props: PropertyDetails[], methods: MethodDetails[], color?: Function) => {
+    let entity = "";
+    if (color) {
+        entity += color(name);
+    } else {
+        entity += name;
+    }
+
+    if (props.length) {
+        entity += `|${props.map(templates.property).join(LINE_SEPARATOR)}`;
+    } else if(methods.length){
+        entity += `| `;
+    }
+
+    if (methods.length) {
+        entity += `|${methods.map(templates.method).join(LINE_SEPARATOR)}`;
+    } else {
+        entity += `|`;
+    }
+
+    return `[${entity}]`;
+};
 
 export const templates = {
     composition: "+->",
     implementsOrExtends: (abstraction: string, implementation: string) => {
         return (
-        `${templates.plainClassOrInterface(abstraction)}` +
-        `^-${templates.plainClassOrInterface(implementation)}`
+            `${templates.plainClassOrInterface(abstraction)}` +
+            `^-${templates.plainClassOrInterface(implementation)}`
         );
     },
     plainClassOrInterface: (name: string) => `[${name}]`,
-    colorClass: (name: string) => `[${name}{bg:skyblue}]`,
-    colorInterface: (name: string) => `[${name}{bg:palegreen}]`,
-    class: (name: string, props: PropertyDetails[], methods: MethodDetails[]) => {
-        const pTemplate = (property: PropertyDetails) => `${property.name};`;
-        const mTemplate = (method: MethodDetails) => `${method.name}();`;
-        return (
-        `${templates.colorClass(name)}` +
-        `[${name}|${props.map(pTemplate).join("")}|${methods.map(mTemplate).join("")}]`
-        );
-    },
+    colorClass: (name: string) => `${name}{bg:skyblue}`,
+    colorInterface: (name: string) => `${name}{bg:palegreen}`,
+    property: (property: PropertyDetails) => property.name,
+    method: (method: MethodDetails) => `${method.name}()`,
+    class: (
+        name: string,
+        props: PropertyDetails[],
+        methods: MethodDetails[]
+    ) => buildEntity(name, props, methods, templates.colorClass),
     interface: (
         name: string,
         props: PropertyDetails[],
         methods: MethodDetails[]
-    ) => {
-        const pTemplate = (property: PropertyDetails) => `${property.name};`;
-        const mTemplate = (method: MethodDetails) => `${method.name}();`;
-        return (
-        `${templates.colorInterface(name)}` +
-        `[${name}|${props.map(pTemplate).join("")}|${methods.map(mTemplate).join("")}]`
-        );
-    }
+    ) => buildEntity(name, props, methods, templates.colorInterface),
 };
